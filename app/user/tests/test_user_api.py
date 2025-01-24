9832cd8c80bbdf1679ad000ceee56e5cbd9f9275
Amir Asaelov <amiras1989@gmail.com>
2025-01-24 08:58:47 +0200

    fix additional whitespace per flake8

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -3,7 +3,7 @@ Tests for the user API
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.urls import  reverse
+from django.urls import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
